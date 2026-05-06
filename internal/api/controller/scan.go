7d2514a680a920f97d99f342949563ab72a94e22
Morteza <mrtdeh@gmail.com>
2026-05-06 18:47:08 +0330

    feat: first implemenet controller methods

--- internal/api/controller/scan.go
+++ internal/api/controller/scan.go
@@ -1,14 +1,36 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"mime/multipart"
 
-type ScanController struct{}
+	"github.com/gin-gonic/gin"
+	"github.com/mrtdeh/scanners-management/internal/api/repositories"
+)
+
+type ScanController struct {
+	ss repositories.ScannerRepository
+}
 
 func NewScanController() *ScanController {
 	return &ScanController{}
 }
 
-func (ctr *ScanController) CreateScan(c *gin.Context)
+type ScanRequest struct {
+	Name        string         `form:"name"`
+	Description string         `form:"description"`
+	File        multipart.File `form:"file"`
+}
+
+func (ctr *ScanController) CreateScan(c *gin.Context) {
+	var req ScanRequest
+
+	if err := c.ShouldBind(&req); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	defer req.File.Close()
+
+}
 func (ctr *ScanController) GetResult(c *gin.Context)
 func (ctr *ScanController) GetStats(c *gin.Context)
 func (ctr *ScanController) GetHistory(c *gin.Context)
